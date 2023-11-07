6e20b5c85bd08da4a932d7e1231531bf126d7c6d
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-07 11:30:59 -0300

    Decimal Field

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="product_brand")
-    price = models.DecimalField()
+    price = models.DecimalField(decimal_places=None, max_digits=10)
     description = models.CharField(max_length=255)
     volume = models.FloatField()
     volume_metric = models.CharField(max_length=50)
