f5f3b99f029e7857e52ece427a9ef51e256fdc93
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-07 11:29:03 -0300

    Finalizing class Product

--- products/models.py
+++ products/models.py
@@ -12,9 +12,13 @@ class Brand(models.Model):
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    brand = models.ForeignKey(Brand)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="product_brand")
     price = models.DecimalField()
     description = models.CharField(max_length=255)
     volume = models.FloatField()
     volume_metric = models.CharField(max_length=50)
+    bars_code = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
 
