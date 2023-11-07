33dcb6aea27d116d7d73923087d4e09d62aeea5a
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-07 11:18:52 -0300

    Creating Models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    brand =
+    price = models.DecimalField()
+    description = models.CharField(max_length=255)
+    volume = models.FloatField()
+    volume_metric = models.CharField(max_length=50)
+
