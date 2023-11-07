357fc63673c8b89f0cb7b9454390aefd7df673ca
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-07 11:21:32 -0300

    Creating Class Brand

--- products/models.py
+++ products/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 
-class Brand
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    brand =
+    brand = models.ForeignKey(Brand)
     price = models.DecimalField()
     description = models.CharField(max_length=255)
     volume = models.FloatField()
