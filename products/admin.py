bd790e4984d6d1279d52c1b62bb842a7734bd2af
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-08 08:57:17 -0300

    Creating Admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from products.models import Product, Brand
 
-# Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'brand',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Brand, BrandAdmin)
