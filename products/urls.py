d1c06a71f8c9e243fddbcd11938134d9e5dd76be
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-09 10:28:57 -0300

    Adding new url for new product to be registered

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from products.views import ProductsList
+from products.views import ProductsList, NewProduct
 
 urlpatterns = [
     path('', ProductsList.as_view(), name='products_view'),
+    path('new_product/', NewProduct.as_view(), name='new_product'),
     ]
