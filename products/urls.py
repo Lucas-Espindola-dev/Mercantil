c185259f4af6233c63b34187ce6f62b04c586c4a
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-13 08:35:09 -0300

    Creating app_name in url.py

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from products.views import ProductsList, NewProduct
 
+app_name = 'products'
+
 urlpatterns = [
     path('', ProductsList.as_view(), name='products_view'),
     path('new_product/', NewProduct.as_view(), name='new_product'),
