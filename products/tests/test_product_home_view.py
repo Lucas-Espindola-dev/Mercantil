687ab39a2d6a362a817a4ec0729b0ef102375c20
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-13 11:10:16 -0300

    test_product_home_view.py class Products View Test

--- products/tests/test_product_home_view.py
+++ products/tests/test_product_home_view.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
-from django.urls import resolve
+from django.urls import resolve, reverse
+from products import views
 
 
 class ProductsViewTest(TestCase):
     def test_products_home_views_Class_is_correct(self):
-        view = resolve('/')
-        self.assertTrue(True)
+        view = resolve(reverse('products:products_view'))
+        self.assertIs(view.func, views.ListView.as_view())
 
