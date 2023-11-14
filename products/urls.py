24b32901c6d4ec78382889a7fc23b9b1a860c1c1
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-14 09:49:20 -0300

    Creating a media root

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from products.views import ProductsList, NewProduct
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductsList.as_view(), name='products_view'),
     path('new_product/', NewProduct.as_view(), name='new_product'),
-    ]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
