af989e159e82dd070816db1d6a6939994a84117c
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-14 09:40:43 -0300

    Correcting Assert Is

--- products/tests/test_product_home_view.py
+++ products/tests/test_product_home_view.py
@@ -6,5 +6,5 @@ from products import views
 class ProductsViewTest(TestCase):
     def test_products_home_views_Class_is_correct(self):
         view = resolve(reverse('products:products_view'))
-        self.assertIs(view.func, views.ListView.as_view())
+        self.assertIs(view.func, views.ListView.as_view)
 
