d3cc1e8ce0e116fd7f9bef6b7482e59d8a83d98b
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-14 10:05:36 -0300

    Adding product detail path in urls

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from products.views import ProductsList, NewProduct
+from products.views import ProductsList, NewProduct, ProductDetail
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -8,4 +8,5 @@ app_name = 'products'
 urlpatterns = [
     path('', ProductsList.as_view(), name='products_view'),
     path('new_product/', NewProduct.as_view(), name='new_product'),
+    path('product/<int:pk>/', ProductDetail.as_view(), name='product_detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
