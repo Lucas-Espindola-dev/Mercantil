c7a60a1c9ac1ade30656c774bd72172c3c29a57b
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-22 09:29:12 -0300

    Creating login view function

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
 
@@ -12,5 +13,20 @@ def register_view(request):
         user_form = UserCreationForm()
     return render(request, 'register.html', {"user_form": user_form})
 
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('products_view')
+        else:
+            login_form = AuthenticationForm()
+
+    else:
+        login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
 
 
