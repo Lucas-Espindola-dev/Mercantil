10a85af7d8de26064bf3adf2a250636af0f3b03c
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-22 09:42:04 -0300

    adding app_name

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from accounts.views import register_view, login_view, logout_view
 
+app_name = 'accounts'
 
 urlpatterns = [
     path('register/', register_view, name='register'),
