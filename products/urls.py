d9f868bdfbff4b680fe991690cd41f7dd855f090
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-23 11:40:31 -0300

    Correcting delete path

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from products.views import ProductsList, NewProduct, ProductDetail, ProductUpdateView, DeleteView
+from products.views import ProductsList, NewProduct, ProductDetail, ProductUpdateView, ProductDeleteView
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path('new_product/', NewProduct.as_view(), name='new_product'),
     path('product/<int:pk>/', ProductDetail.as_view(), name='product_detail'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
-    path('product/<int:pk>/delete/', DeleteView.as_view(), name='product_delete'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
