c6f246f09dfd51fe7fc7d90f4b6fcea42320a855
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-23 09:39:20 -0300

    Adding update route

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from products.views import ProductsList, NewProduct, ProductDetail
+from products.views import ProductsList, NewProduct, ProductDetail, ProductUpdateView
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -8,5 +8,6 @@ app_name = 'products'
 urlpatterns = [
     path('', ProductsList.as_view(), name='products_view'),
     path('new_product/', NewProduct.as_view(), name='new_product'),
-    path('product/<int:pk>/', ProductDetail.as_view(), name='product_detail')
+    path('product/<int:pk>/', ProductDetail.as_view(), name='product_detail'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
